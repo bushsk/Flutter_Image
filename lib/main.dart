8cb23eeedd21de94830165db2e522e25cdca4be4
bushra <bushra.sk@siesgst.ac.in>
2022-01-28 11:17:15 +0530

    image_with_listview

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,11 @@ class MyApp extends StatelessWidget {
   }
 }*/
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -36,22 +38,22 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo', theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-      home: MyHomePage(title: 'Product layout demo home page'),
+      home: const MyHomePage(title: 'Product layout demo home page'),
     );
   }
 }
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Product Listing")),
+        appBar: AppBar(title: const Text("Product Listing")),
         body: ListView(
           shrinkWrap: true,
           padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
-          children: <Widget>[
+          children: const <Widget>[
             ProductBox(
                 name: "iPhone",
                 description: "iPhone is the stylist phone ever",
@@ -94,16 +96,17 @@ class MyHomePage extends StatelessWidget {
   }
 }
 class ProductBox extends StatelessWidget {
-  ProductBox({Key? key, required this.name, required this.description, required this.price, required this.image}) :
+  const ProductBox({Key? key, required this.name, required this.description, required this.price, required this.image}) :
         super(key: key);
   final String name;
   final String description;
   final int price;
   final String image;
 
+  @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(2),
+        padding: const EdgeInsets.all(2),
         height: 120,
         child: Card(
             child: Row(
@@ -112,17 +115,15 @@ class ProductBox extends StatelessWidget {
                   Image.asset("assets/" + image),
                   Expanded(
                       child: Container(
-                          padding: EdgeInsets.all(5),
+                          padding: const EdgeInsets.all(5),
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: <Widget>[
                               Text(
-                                  this.name, style: TextStyle(
-                                  fontWeight: FontWeight.bold
-                              )
+                                  name, style: const TextStyle(fontWeight: FontWeight.bold)
                               ),
-                              Text(this.description), Text(
-                                  "Price: " + this.price.toString()
+                              Text(description), Text(
+                                  "Price: " + price.toString()
                               ),
                             ],
                           )
